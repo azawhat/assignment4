96bc56656d9ea6713cb9d163b7c148fa811f7856
azawhat <azamat.erezhepbayev@gmail.com>
2023-05-13 18:33:15 +0600

    Defining variables size and buckets

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,5 +1,9 @@
+import java.util.Map;
+
 public class MyHashTable<K, V> {
     private static final int capacity = 16;
     private static final double load_factor = 0.75;
 
+    private List<Entry<K,V>>[] buckets;
+    private int size;
 }
