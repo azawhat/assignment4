82cc156228f3304743f0951d8624d67c190bf969
azawhat <azamat.erezhepbayev@gmail.com>
2023-05-13 18:28:54 +0600

    Creating hashCode

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -12,4 +12,11 @@ public class MyTestingClass {
     public void setValue(int value) {
         this.value = value;
     }
+
+    @Override
+    public int hashCode(){
+        int result = 17;
+        result = 31*result + value;
+        return result;
+    }
 }
