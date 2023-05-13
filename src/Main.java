d9e7b10baccc7dc5055ec2a60de98d377bed0832
azawhat <azamat.erezhepbayev@gmail.com>
2023-05-13 18:57:22 +0600

    Adding 10 000 elements

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args)
     {
-
+        MyHashTable<MyTestingClass, String> table = new MyHashTable<>();
+        for (int i = 0; i<10000; i++){
+            MyTestingClass key  = new MyTestingClass(i);
+            table.put(key, "value" + i);}
     }
 }
